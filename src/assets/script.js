8687a272dcf1145a4dc4a454ea89423ca46b63fe
wibawasuyadnya <dekgusnfs@gmail.com>
2022-10-18 10:10:37 +0800

    commit secondary

---
+++ src/assets/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    var reviews = [];
+    $.getJSON('src/assets/data-reviews.json', function(data) {
+        $.each(data.reviews, function(i, f) {
+           var tblRow = "<tr>" + "<td>" + f.description + "</td>" +
+            "<td>" + f.title + "</td>" + "<td>" + f.product + "</td>" + "<td>" + f.date + "</td>" + "</tr>"
+            $(tblRow).appendTo("#datareview tbody");
+      });
+ 
+    });
+ 
+ });
